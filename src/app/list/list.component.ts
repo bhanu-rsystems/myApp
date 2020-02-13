a98b74b043e51b6a7ef1b87608d5ff6b452b5213
Bhanu Pratap <Bhanu.Pratap@rsystems.com>
2020-02-13 18:29:00 +0530

    comit myApp

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+
+	brews: object;
+
+  constructor(private _http: HttpService ) { }
+
+  ngOnInit(): void {
+  	this._http.myMethod().subscribe( data => {
+  		this.brews = data;
+  	});
+  }
+
+}
